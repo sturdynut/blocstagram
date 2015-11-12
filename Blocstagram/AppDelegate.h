dad3e953623b36b0c3f321e45e5a606134cd94ca
Matti Salokangas <matti.salokangas@gmail.com>
2015-11-11 16:40:20 -0800

    Add initial project files

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Matti Salokangas on 11/11/15.
+//  Copyright © 2015 Sturdy Nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Matti Salokangas on 11/11/15.
+//  Copyright © 2015 Sturdy Nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
